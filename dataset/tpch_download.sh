d1ae713b9deebd4b915604d5a5479f990de06d8d
刘良祖 <110663967+liuliangzu@users.noreply.github.com>
2024-05-19 22:45:50 +0800

    Update tpch_download.sh

--- dataset/tpch_download.sh
+++ dataset/tpch_download.sh
@@ -1,1 +1,11 @@
-
+sudo yum install git make gcc
+git clone https://github.com/gregrahn/tpch-kit.git
+cd tpch-kit/dbgen
+make MACHINE=LINUX DATABASE=POSTGRESQL
+export DSS_CONFIG=/.../tpch-kit/dbgen
+export DSS_QUERY=$DSS_CONFIG/queries
+export DSS_PATH=/path-to-dir-for-output-files
+dbgen -h 
+# generate data
+qgen -v -c -d -s 1 > tpch-stream.sql
+# generate query
