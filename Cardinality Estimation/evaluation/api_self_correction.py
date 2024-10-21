8af61238d76aa1b6c7b11522977fff8a27b2aa45
刘良祖 <110663967+liuliangzu@users.noreply.github.com>
2024-10-21 10:33:36 +0800

    Create api_self_correction.py

---
+++ Cardinality Estimation/evaluation/api_self_correction.py
@@ -0,0 +1,123 @@
+from openai import OpenAI
+import json
+import pandas as pd
+import re
+imdb = "other optimizer prediction"
+def get_ndv_predict():
+    res = []
+    with open('/mnt/bd/llama-finetune/pilotscope/ndv_job_light.txt','r') as file:
+        lines_sql = file.readlines()
+    for line in lines_sql:
+        s = line.replace('\n','').split(',')[-1]
+        numbers = re.findall(r'\d+', s)
+        res.append(int(numbers[0]))
+    return res
+client = OpenAI(base_url="http://localhost:8000/v1",api_key="EMPTY")
+with open('/mnt/bd/llama-finetune/pilotscope/stats-icl.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+with open('/mnt/bd/llama-finetune/llama3/PRICE/genome_test.txt','r') as file:
+    lines_sql = file.readlines()
+pr_res = imdb
+'''file_path ='/mnt/bd/llama-finetune/llama3/ALECE/data/STATS/workload/u_heavy/workload.sql'
+with open(file_path, 'r') as f:
+    lines_sql = f.readlines()
+lines_r = []
+pg_ce=[]
+truth = []
+for line in lines_sql:
+    query_type = line.split(": ")
+    if len(query_type) < 2:
+        continue
+    if query_type[0].count("train"):
+        if query_type[0].count("train_sub_query") == 0:
+            pg_ce.append(query_type[1].split('||')[2])
+            truth.append(query_type[1].split('||')[3])
+        else:
+            pg_ce.append(query_type[1].split('||')[1])
+            truth.append(query_type[1].split('||')[2])'''
+#pr_res = get_ndv_predict()
+results_list = []
+chat = 0
+for i in range(len(data)):# ,just give a number as the answer and don't say anything else
+#for i in range(3):# ,just give a number as the answer and don't say anything else
+    q_error = 100
+    other_prompt = ""
+    message_history = [{"role": "system", "content": str(data[i]['instruction'])},{"role": "user", "content": data[i]['input']}]
+    res = 0
+    flag = 0
+    max_chat = 0
+    if chat == 1:
+        while q_error > 2 and max_chat < 5:
+            if len(other_prompt) > 5:
+                flag = 1
+                message_history.append(
+                        {"role": "user", "content": other_prompt}
+                    )
+            response = client.chat.completions.create(
+                model='llama',
+                messages=message_history,
+                max_tokens=15,
+                temperature=0.7,
+                stream=False
+            )
+            max_chat = max_chat + 1
+            llm_res = response.choices[0].message.content
+            print(llm_res)
+            if llm_res[0] == " ":
+                llm_res = llm_res.split(" ")[1]
+            if float(pr_res[i]) < 1:
+                pr_res[i] = 1
+            else:
+                pr_res[i] = int(float(pr_res[i]))
+            message_history.append({"role": "assistant", "content": llm_res})
+            #pr_res[i] = max(float(data[i]['output']) / 5.0,1.0)
+            try:
+                t = float(llm_res.split(" ")[0].split("\n")[0].split(",")[0])
+            except:
+                break
+            if float(pr_res[i]) > float(llm_res.split(" ")[0].split("\n")[0].split(",")[0]):
+                if float(llm_res.split(" ")[0].split("\n")[0].split(",")[0]) == 0:
+                    q_error = float(pr_res[i])/1
+                else:  
+                    q_error = float(pr_res[i])/float(llm_res.split(" ")[0].split("\n")[0].split(",")[0])
+            else:
+                q_error = float(llm_res.split(" ")[0].split("\n")[0].split(",")[0])/float(pr_res[i])
+            #other_prompt = "The other optimizier estimate this query with {}, and you should give your estimation again as a number".format(pr_res[i])
+            other_prompt = "The other optimizier estimate this query with {}, then you should give your estimation again as a number".format(pr_res[i])
+            if flag == 1:
+                res = (float(llm_res.split(" ")[0].split("\n")[0].split(",")[0]) + float(pr_res[i]))/2
+            else:
+                res = float(llm_res.split(" ")[0].split("\n")[0].split(",")[0])
+            #res = (float(llm_res.split(" ")[0].split("\n")[0].split(",")[0])+ float(pr_res[i]))/2
+        results_list.append([res,data[i]['output']])
+        print(float(pr_res[i]),res,data[i]['output'])
+    else:
+        response = client.chat.completions.create(
+                model='deepseek',
+                messages=message_history,
+                max_tokens=15,
+                temperature=0.7,
+                stream=False
+            )
+        llm_res = response.choices[0].message.content
+        results_list.append([llm_res,data[i]['output']])
+        print(llm_res,data[i]['output'])
+result_dataframe = pd.DataFrame(results_list,columns=["preds","true"])
+result_dataframe.to_csv('./api_icl_stats_y_llama3.csv',index=False)
+'''for i in range(len(data)):# ,just give a number as the answer and don't say anything else
+    response = client.chat.completions.create(
+        model='llama3',
+        messages=[
+            {"role": "system", "content": str(data[i]['instruction'])},
+            {"role": "user", "content": data[i]['input']},
+        ],
+        max_tokens=20,
+        temperature=0.7,
+        stream=False
+    )
+    
+    results_list.append([response.choices[0].message.content,data[i]['output']])
+    
+    print(results_list[i])
+result_dataframe = pd.DataFrame(results_list,columns=["preds","true"])
+result_dataframe.to_csv('./api_price+llm3_job-light.csv',index=False)'''
